be2770dae0ed1c5e98442c1616c56cb570f802b2
Dhananjay Balan <mail@dbalan.in>
2021-08-01 01:38:09 +0200

    Config file support.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,47 @@
+{-# LANGUAGE ApplicativeDo     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Main where
 
-import Byline
-import Control.Applicative
-import Control.Monad.IO.Class
-import Data.Text as T
-import Options.Applicative
-import RtmAPI
+import           Byline
+import           Config.Schema
+import           Control.Applicative
+import           Control.Exception (throw, try)
+import           Control.Monad.IO.Class
+import           Data.Text as T
+import qualified Data.Text.IO as TIO
+import           Options.Applicative
+import           RtmAPI
+import           System.FilePath
+
 
 data Cmd =
     LsAll
   | Configure
   | Query Text
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq)
 
-argParser :: Parser Cmd
-argParser = subparser
+data Args = Args
+  { config :: FilePath
+  , cmd :: Cmd }
+  deriving (Show, Eq)
+
+data Config = Config
+  { cfApikey :: Text
+  , cfSecret :: Text
+  , cfToken :: Text }
+  deriving (Show, Eq)
+
+configSpec :: ValueSpec Config
+configSpec = sectionsSpec "" $ do
+  cfApikey <- reqSection "apikey" "API key of the Application"
+  cfSecret <- reqSection "secret" "API shared secret"
+  cfToken <- reqSection "token" "Application token"
+  pure Config{..}
+
+cmdParser :: Parser Cmd
+cmdParser = subparser
   (  command "configure" (info (pure Configure) (progDesc "configure application access (mostly just for first time use)"))
   <> command "list" (info (pure LsAll) (progDesc "list all tasks"))
   <> command "query" (info qry (progDesc "query for specific tasks"))
@@ -24,22 +50,58 @@ argParser = subparser
 qry :: Parser Cmd
 qry = Query <$> argument str (metavar "QUERY")
 
-opts :: ParserInfo Cmd
+-- Applicative do is magic
+argParser :: Parser Args
+argParser = do
+  config <- strOption
+              ( long "config-file"
+             <> short 'c'
+             <> metavar "FILE"
+             <> value "/home/dj/.config/rmm/config.ini" )
+  cmd <- cmdParser
+  pure Args{..}
+
+opts :: ParserInfo Args
 opts = info (argParser <**> helper)
   ( fullDesc
   <> progDesc "CLI Companion for RTM"
   <> header "So that you don't have to remember" )
 
+readConfig :: FilePath -> IO Config
+readConfig path = do
+  cfg <- try $ loadValueFromFile configSpec path
+  case (cfg :: Either IOError Config) of
+    Left e -> do
+      -- no config file, ask user to run configurn
+      runBylineT $ sayLn (("Configuration file not found at: " <> text (T.pack . show $ path) <> fg red)
+                        <> "\nRunning configure")
+      configure path
+      readConfig path
+    Right cfg' -> pure cfg'
+
+writeConfig :: FilePath -> Text -> Text -> Text -> IO ()
+writeConfig path apikey secret token = do
+  let c' =
+        "apikey: \"" <> apikey <> "\"\n" <>
+        "secret: \"" <> secret <> "\"\n" <>
+        "token:  \"" <> token <> "\"\n"
+  TIO.writeFile path c'
+
 main :: IO ()
 main = do
   args <- execParser opts
-  -- configure
-  list
+  -- read config
+  cfg <- readConfig (config args)
+  case (cmd args) of
+    Configure -> configure (config $ args)
+    LsAll     -> list cfg "dueBefore:tomorrow NOT status:complete"
+    Query q   -> list cfg q
 
-configure :: IO ()
-configure = do
-  let secret = "10fa10b9a5f290fe"
-      apiKey = "76cd370db1c53c387a1aaec1058ffaba"
+
+configure :: FilePath -> IO ()
+configure path = do
+  let secret = "10fa10b9a5f290fe" :: Text
+      apiKey = "76cd370db1c53c387a1aaec1058ffaba" :: Text
   frob <- getFrob secret apiKey
   case frob of
       Nothing -> do
@@ -49,19 +111,21 @@ configure = do
         let loginurl = buildLoginUrl secret apiKey f'
         runBylineT $ do
           sayLn (("Please visit following URL to login: " <> fg green) <> (text loginurl <> fg red))
-          askLn ("Press enter when done!" <> fg blue) Nothing
+          askLn ("Press enter when done!\n" <> fg blue) Nothing
           pure ()
-
         resp <- getToken secret apiKey f'
-        putStrLn $ show resp
-  pure ()
+        case resp of
+          Nothing -> do
+            runBylineT $ sayLn ("Failed generating login url" <> fg red)
+            pure ()
+          Just (Auth t p id u fn) -> writeConfig path apiKey secret t
+          Just _ -> do
+            runBylineT $ sayLn ("Something terrible" <> fg red)
+            pure ()
 
-list :: IO ()
-list = do
-  let secret = "10fa10b9a5f290fe"
-      apiKey = "76cd370db1c53c387a1aaec1058ffaba"
-      token = "b8a07b913f0bdb15f7e09b3c3f9fa1c4454d1cf7"
-      filter = "dueBefore:tomorrow NOT status:complete" :: Text
-  ts <- getTasks secret apiKey token filter
+
+list :: Config -> Text -> IO ()
+list cfg filter = do
+  ts <- getTasks (cfSecret cfg) (cfApikey cfg) (cfToken cfg) filter
   putStrLn $ show $ ts
 
